7a0c241ab09d97a6a94aa735b6a31276d27fe4ea
Cole Johnson <coledjohn@gmail.com>
2026-03-05 00:04:51 -0700

    Pretty print board using ANSICodes, impl proper mod

--- src/main/board.kt
+++ src/main/board.kt
@@ -8,8 +8,10 @@ class Board(var rows: Int, var cols: Int, var numPoints: Int = 3) {
   init {
     // make random konway cells alive
     for (p in 0..numPoints) {
+      // generate random point indices on the board
       var i = Random.Default.nextInt() % rows
       var j = Random.Default.nextInt() % cols
+      // clamp to positive vals
       i = if (i > 0) i else -i
       j = if (j > 0) j else -j
       tiles[i][j] = State.ALIVE
@@ -19,9 +21,23 @@ class Board(var rows: Int, var cols: Int, var numPoints: Int = 3) {
   fun display() {
     for (i in 0..rows - 1) {
       for (j in 0..cols - 1) {
-        print(tiles[i][j].str)
+        val state = tiles[i][j]
+        when (state) {
+          State.ALIVE -> {
+            print(ANSICodes.YELLOW_BEGIN.code)
+            print(state.str)
+            print(ANSICodes.YELLOW_END.code)
+          }
+          State.DEAD -> {
+            print(state.str)
+          }
+        }
       }
       println()
     }
   }
+}
+
+fun Mod(a: Int, b: Int): Int {
+  return (a % b + b) % b
 }
