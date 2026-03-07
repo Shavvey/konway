1fe99618efd2bc8a20f2f48312ec1c0cdf6e6277
Cole Johnson <coledjohn@gmail.com>
2026-03-07 16:04:38 -0700

    Add features to `display()`, use glider as example

--- src/main/board.kt
+++ src/main/board.kt
@@ -2,32 +2,43 @@ package src.main
 
 class Board(var rows: Int, var cols: Int) {
   var tiles: Array<Array<State>> = Array(rows) { Array(cols) { State.DEAD } }
+  var generations = 0
 
   init {
-    // Example initial board state, 2 period blinker
-    tiles[1][2] = State.ALIVE
-    tiles[2][2] = State.ALIVE
+    tiles[3][1] = State.ALIVE
     tiles[3][2] = State.ALIVE
+    tiles[3][3] = State.ALIVE
+    tiles[2][3] = State.ALIVE
+    tiles[1][2] = State.ALIVE
   }
 
   fun display() {
     print(ANSICodes.CLEAR_SCREEN.code)
-    for (i in 0..rows - 1) {
-      for (j in 0..cols - 1) {
-        val state = tiles[i][j]
-        when (state) {
+    tiles.forEach {
+      it.forEach {
+        when (it) {
           State.ALIVE -> {
             print(ANSICodes.YELLOW_BEGIN.code)
-            print(state.str)
+            print(it.str)
             print(ANSICodes.COLOR_END.code)
           }
           State.DEAD -> {
-            print(state.str)
+            print(it.str)
           }
         }
       }
       println()
     }
+    val pop = get_population()
+    println("Population: $pop")
+    println("Generation: $generations")
+    println("(Press 'q' to quit)")
+  }
+
+  fun get_population(): Int {
+    var population = 0
+    tiles.forEach { it.forEach { if (it == State.ALIVE) population += 1 } }
+    return population
   }
 
   fun update() {
@@ -57,6 +68,7 @@ class Board(var rows: Int, var cols: Int) {
         }
       }
     }
+    generations += 1
   }
 
   // Return all the neighbors for each cell inside the board
