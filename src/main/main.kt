797b65ae29e45ef406db09d83b2dff42b2484524
Cole Johnson <coledjohn@gmail.com>
2026-03-07 14:23:47 -0700

    Make non-blocking key escape from program

--- src/main/main.kt
+++ src/main/main.kt
@@ -1,12 +1,19 @@
 package src.main
 
 import java.lang.Thread
+import java.util.*
 
 fun main() {
   val board = Board(5, 5)
-  while (true) {
+  do {
+    var key = '!'
+    // If no input is found, continue updating and displaying board
     board.update()
     board.display()
+    // Non-blocking call that estimates bytes to be read from stdin
+    if (System.`in`.available() > 0) {
+      key = System.`in`.read().toChar()
+    }
     Thread.sleep(1_000L)
-  }
+  } while (key != 'q')
 }
